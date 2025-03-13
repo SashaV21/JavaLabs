46ee61c464e002ca3705997a1618fbcff22c9fdc
SashaV21 <AleksandrAleksandr450@gmail.com>
2025-03-13 20:05:29 +0300

    Done third task

---
+++ src/main/java/Lab1/Task3.java
@@ -0,0 +1,25 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите числа через пробел: ");
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        int third = scanner.nextInt();
+
+        int max = first;
+        if (max < second){
+            max = second;
+        }
+        if (max < third){
+            max = third;
+        }
+        System.out.println("Максимальное значение найденное с помощью условного оператора: " + max);
+        max = 0;
+        max = Math.max(Math.max(first, second), third);
+        System.out.println("Максимальное значение с помощью Math.max(): " + max);
+    }
+}
